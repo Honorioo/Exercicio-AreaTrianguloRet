e5fc40f979f22162467f595b5b45f008066b1b1a
Vinicius <viniciusgomeshonorio@hotmail.com>
2021-03-21 15:33:32 -0300

    Exercicio AreaTrianguloRet

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercicio_AreaTrianguloRet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double Base , Altura, resultado;
+
+
+            Console.Write("-------Área de Um Triangulo Retangulo-------");
+            Console.WriteLine(); 
+            
+            Console.Write("Digite a base: "); 
+            Base = Convert.ToDouble(Console.ReadLine());
+            
+                Console.Write("Digite a Altura: ");
+                Altura = Convert.ToDouble(Console.ReadLine());
+
+
+                resultado = (Base*Altura)/2;
+                Console.WriteLine($"Resultado da Área: {resultado}");
+
+
+        }
+    }       
+}    
